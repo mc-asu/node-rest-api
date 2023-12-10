155d5e89204628f40456e020e1d72d80630ca8d5
Manuel Casupanan <manuel.casupanan@jmarquardt.com>
2023-12-10 23:17:51 +0100

    first commit

---
+++ routes/feed.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const feedController = require('../controller/feed')
+
+const router = express.Router()
+
+// GET /feed/posts
+router.get('/posts', feedController.getPosts)
+
+// POST /feed/posts
+router.post('/posts', feedController.createPost)
+
+module.exports = router
