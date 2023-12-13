b42d101d812e63b80d98461cfca4f0afa85f6a67
Manuel Casupanan <manuel.casupanan@jmarquardt.com>
2023-12-13 18:48:28 +0100

    sorting changed

--- controller/feed.js
+++ controller/feed.js
@@ -14,6 +14,7 @@ exports.getPosts = async (req, res, next) => {
         const totalItems = await Post.find().countDocuments()
         const posts = await Post.find()
             .populate('creator')
+            .sort({createdAt: -1})
             .skip((currentPage - 1) * PER_PAGE)
             .limit(PER_PAGE)
         statusHandler.success(res, 200, { 
